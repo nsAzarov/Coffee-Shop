5a15e7139115f89a9cb36d783d719e9793a80475
nsAzarov <nsazarov99@gmail.com>
2020-02-03 02:58:14 +0300

    premium section

---
+++ client/src/Components/Product/Content/Premium.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import styled from 'styled-components';
+const img = require(`../../../images/premium.jpg`);
+
+const TextWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: white;
+    width: 400px;
+    margin-left: 80px;
+    .premium-heading {
+        margin-bottom: 10px;
+        font-size: 24px;
+        line-height: 36px;
+        font-weight: 400;
+        span {
+            font-weight: 700;
+        }
+    }
+    p {
+        opacity: 0.9;
+        margin-bottom: 30px;
+        font-size: 18px;
+        line-height: 30px;
+    }
+    .premium-content-article {
+        display: flex;
+        .icon {
+            display: flex;
+            width: 50px;
+            height: 28px;
+            margin-right: 20px;
+            justify-content: center;
+            align-items: center;
+            img {
+                display: inline-block;
+                max-height: 28px;
+                max-width: 100%;
+                margin-top: 25px;
+                margin-bottom: 25px;
+            }
+        }
+        .text {
+            .heading {
+                font-size: 16px;
+                line-height: 28px;
+                font-weight: 400;
+            }
+            p {
+                opacity: .7;
+                font-size: 16px;
+            }
+        }
+    }
+`;
+
+const ImageWrap = styled.div`
+    display: inline-block;
+    height: 100%;
+    width: 50%;
+    padding-right: 10px;
+    background: url(${img});
+    background-position: 50% 50%;
+    background-size: cover;
+`;
+
+const PremiumSection = styled.div`
+    display: flex;
+    height: 460px;
+    width: 100%;
+    background-color: #1d1f2e;
+`;
+
+export default function Premium() {
+    return (
+        <PremiumSection>
+            <ImageWrap />
+            <TextWrap>
+                <div className='premium-heading'>Handmade by <span>CoffeeShop.</span></div>
+                <p>The most versatile furniture system ever created. Designed to fit your life.</p>
+                <div className="premium-content-article">
+                    <div className="icon">
+                        <img src={require(`../../../images/diamond.png`)} alt=''/>
+                    </div>
+                    <div className="text">
+                        <div className="heading">Premium Quality</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in.</p>
+                    </div>
+                </div>
+                <div className="premium-content-article">
+                    <div className="icon">
+                        <img src={require(`../../../images/lamp.png`)} alt=''/>
+                    </div>
+                    <div className="text">
+                        <div className="heading">Gentle to the Environment</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in.</p>
+                    </div>
+                </div>
+            </TextWrap>
+        </PremiumSection>
+    )
+}
--- client/src/Components/Product/Content/index.js
+++ client/src/Components/Product/Content/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import {Content} from '../../Other/ContentWrap';
 import ProductInfo from './ProductInfo';
+import Premium from './Premium';
 
 export default function index() {
     return (
         <Content>
             <ProductInfo />
+            <Premium />
         </Content>
     )
 }
